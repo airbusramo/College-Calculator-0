20c6653cb43769f3a8c69d6b912ddd97d6e5efd3
airbusramo <omarrefaat15@live.com>
2019-11-16 19:41:59 +0200

    Add project files.

---
+++ College-Calculator-0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace College_Calculator_0
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("This program will add two numbers together and perfrom an operation of your choosing.");
+
+            string operationText =
+@"1: Addition
+2: Subtraction
+";
+
+            //Inputs
+            Console.WriteLine("Enter First Number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Second Number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Select your operation.");
+            Console.WriteLine();
+            int operation;
+        }
+    }
+}
